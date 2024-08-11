7c39b606c773887bda4855fedf18d5bdb75a2d8b
jasmaa <jasonmaa3955@gmail.com>
2024-08-11 13:33:22 -0400

    Add main function to dedupe script

--- scripts/dedupe.py
+++ scripts/dedupe.py
@@ -1,25 +1,33 @@
 import csv
 import tempfile
 
-with tempfile.NamedTemporaryFile("r+", newline="") as tempf:
-    writer = csv.writer(tempf)
 
-    # Go through fortunes and dedupe rows based on fortune
-    with open("fortunes.csv", "r", newline="") as rf:
-        reader = csv.reader(rf)
+def main():
+    """Dedupe rows in fortunes.csv based on fortune.
+    """
+    with tempfile.NamedTemporaryFile("r+", newline="") as tempf:
+        writer = csv.writer(tempf)
 
-        # Skip header
-        header_row = reader.__next__()
-        writer.writerow(header_row)
+        # Go through fortunes and dedupe rows based on fortune
+        with open("fortunes.csv", "r", newline="") as rf:
+            reader = csv.reader(rf)
 
-        fortune_set = set()
-        for row in reader:
-            if row[0] not in fortune_set:
-                fortune_set.add(row[0])
-                writer.writerow(row)
+            # Skip header
+            header_row = reader.__next__()
+            writer.writerow(header_row)
 
-    # Re-wind and write to fortunes
-    tempf.seek(0)
-    with open("fortunes.csv", "w", newline="") as wf:
-        for line in tempf:
-            wf.write(line)
+            fortune_set = set()
+            for row in reader:
+                if row[0] not in fortune_set:
+                    fortune_set.add(row[0])
+                    writer.writerow(row)
+
+        # Re-wind and write to fortunes
+        tempf.seek(0)
+        with open("fortunes.csv", "w", newline="") as wf:
+            for line in tempf:
+                wf.write(line)
+
+
+if __name__ == "__main__":
+    main()
